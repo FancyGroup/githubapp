73f9ac72bd6d078aa102a1f6ee3f7c017fd02565
艾伦 <allenps1991@hotmail.com>
2016-09-30 14:20:34 +0800

    remove sub module

--- README.md
+++
@@ -1,3 +0,0 @@
-## webstorm sass 配置
-npm install -g node-sass
-http://stackoverflow.com/questions/16466023/how-do-i-correctly-setup-the-scss-file-watcher-in-phpstorm
--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/gulp/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/orchestrator/index.d.ts" />
-/// <reference path="globals/q/index.d.ts" />
-/// <reference path="globals/vinyl/index.d.ts" />
